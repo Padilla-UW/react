9d5cdd0fca127e07826b60aea7abacc9e51d77f1
Padilla <hector.padilla@uwipes.com>
2022-11-15 17:49:10 -0600

    useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const CounterApp =({value})=>{
+export const CounterApp =({ value })=>{
+
+    const [counter, setCounter] = useState(10);
+
+    const handleAdd = (event) =>{
+        setCounter(counter + 1);
+    }
+
     return <>
-    <h1>  CounterApp</h1>   
-    <h3>  {value}</h3>   
+        <h1> CounterApp </h1>   
+        <h3> { counter } </h3>   
+        <button onClick={handleAdd} >
+             + 1 
+        </button>
     </> 
 }
 
