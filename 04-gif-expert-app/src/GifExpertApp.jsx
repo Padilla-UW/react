739cde33a27177726151df481b6d9c0db5d003b3
Padilla <hector.padilla@uwipes.com>
2022-12-09 13:19:10 -0600

    Creando lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
 
 export const GifExpertApp = ()=>{
+
+    const [categories, setCategories] = useState(['one punch','Dragon']);
     return(
         <>
         <h1>GifExpert</h1>
+
+        <ol>
+            { categories.map(category => {
+                return <li key={category}>{category}</li>
+            })}
+        </ol>
         </>
     )
 }
+
+// QyeV9UbGybnRfIRFeypCykiIPIdwqADg APIKEY
