534436ecfe83f6711e6c8dca2cd5b9b243279bde
Padilla <hector.padilla@uwipes.com>
2022-12-16 13:49:07 -0600

    Pruebas GifGrindItem

--- 04-gif-expert-app/src/test/components/GifItem.test.jsx
+++ 04-gif-expert-app/src/test/components/GifItem.test.jsx
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../components"
 
 describe('Pruebas en GifItem',()=>{
     const title = 'one punch';
-    const url = 'http://one-punch.com';
+    const url = 'http://one-punch.com/img.jpg';
 
     test('Debe hacer match con el snapshot', () => { 
         const {container} = render(<GifItem 
@@ -12,4 +12,25 @@ describe('Pruebas en GifItem',()=>{
           />)
           expect(container).toMatchSnapshot();
     })
+
+    test('Debe mostrar la imagen con el url y alt asignados', () => {
+        render(<GifItem 
+            title={title}
+            url={url}  
+          />);
+
+        const { src, alt } = screen.getByRole('img');
+        //expect(screen.getByRole('img').src).toBe(url);
+        expect(src).toBe(url);
+        expect(alt).toBe(title);    
+
+     });
+
+     test('Debe de agregar el titulo de la imagen',()=>{
+        render(<GifItem 
+            title={title}
+            url={url}  
+          />);
+        expect(screen.getByText(title)).toBeTruthy();
+     })
 })
