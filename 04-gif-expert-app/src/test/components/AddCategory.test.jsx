359a3da283f6ca93cca9aef347def54853f66d69
Padilla <hector.padilla@uwipes.com>
2022-12-17 10:37:15 -0600

    Pruebas jest function

--- 04-gif-expert-app/src/test/components/AddCategory.test.jsx
+++ 04-gif-expert-app/src/test/components/AddCategory.test.jsx
@@ -9,4 +9,32 @@ describe('Pruebas de <AddCategory/>', () => {
         expect(input.value).toBe('Saitama');
 
      })
+
+    test('debe llamar a la funcion onNewCategory', () => { 
+            const inputValue = 'Saitama';
+            const onNewCategory = jest.fn();
+
+
+            render(<AddCategory onNewCategory={onNewCategory}/>);
+            const input = screen.getByRole('textbox');
+            const form = screen.getByRole('form');
+
+            fireEvent.input(input,{target:{value:'Saitama'}});
+            fireEvent.submit(form);
+
+            expect(input.value).toBe('');
+            expect(onNewCategory).toHaveBeenCalled();
+            expect(onNewCategory).toHaveBeenCalledTimes(1);
+            expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+     })
+
+     test('No debe llamar al onNewCategory si el input está vacio', () => { 
+        const onNewCategory = jest.fn();
+
+
+        render(<AddCategory onNewCategory={onNewCategory}/>);
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+      })
 })
