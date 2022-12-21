3404bb4c4eb0660a25aa73b2a4dc52a829fbc2f6
Padilla <hector.padilla@uwipes.com>
2022-12-21 17:58:49 -0600

    dependencias useEffect

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username : 'HPadilla',
+        email: 'hector.padilla@uwipes.com'
+    })
+
+    const {username, email} = formState;
+
+    const onInputChange = ({target})=>{
+        const {name,value} = target;
+        setFormState({...formState, 
+            [name]:value});
+    }
+
+    useEffect(() => {
+     console.log('Cambio el formulario');
+    }, [formState])
+    
+    useEffect(() => {
+     console.log('Cambio el correo');
+    }, [email])
+    
+
+  return (
+    <>
+        <h1>Formulario simple</h1>
+        <hr />
+        <input  type="text" 
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange} />
+        
+        <input  type="email" 
+                className="form-control mt-2"
+                placeholder="hector.padilla@uwipes.com"
+                name="email"
+                value={email}
+                onChange={onInputChange} />
+    </>
+  )
+}
