e8aef365927834fd7def2ae09e98554242be1c38
Padilla <hector.padilla@uwipes.com>
2022-12-21 13:19:04 -0600

    counterAPP

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CounterApp } from './01-useState/CounterApp';
+//import HooksApp from './HooksApp';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CounterApp/>
   </React.StrictMode>,
 )
