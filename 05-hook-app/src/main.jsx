48c5ed2df713d2499b9bbd9de3e9d28cc1058998
Padilla <hector.padilla@uwipes.com>
2022-12-22 17:20:36 -0600

    useFetch-customHook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom/client';
 //import { CounterWithCustomHook } from './01-useState/counterWithCustomHook';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import HooksApp from './HooksApp';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx';
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook/>
+    <MultipleCustomHooks/>
   </React.StrictMode>,
 )
