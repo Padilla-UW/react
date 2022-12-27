19b253b43bb3461d5eb96442861c985fde86e5b3
Padilla <hector.padilla@uwipes.com>
2022-12-27 17:34:42 -0600

    useFetch + useCounter

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,12 +1,29 @@
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "./";
+
 
 export const MultipleCustomHooks = () => {
-    const { data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
-    console.log(data, isLoading,hasError);
+  const { counter, increment} = useCounter(1);
+  const { data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/'+counter);
+  const { quote, author} = !!data&&data[0];
+    
     return (
     <>
         <h1>BreakingBad Quotes</h1>
         <hr />
+
+
+      {
+        isLoading
+        ?<LoadingQuote/>
+        :<Quote quote={quote} author={author} /> 
+      }
+
+      <button onClick={()=>increment()}
+      disabled = {isLoading} className="btn btn-primary">Next quote</button>
+        
+
+        
     </>
   )
 }
