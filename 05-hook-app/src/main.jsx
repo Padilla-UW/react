f1f639d0806aae57a7637e3913e9fc884fbe23b8
Padilla <hector.padilla@uwipes.com>
2022-12-28 11:44:38 -0600

    memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -7,12 +7,13 @@ import ReactDOM from 'react-dom/client';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx';
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx';
 //import { FocusScreen } from './04-useRef/FocusScreen.jsx';
-import { Layout } from './05-useLayoutEffect/Layout.jsx';
+//import { Layout } from './05-useLayoutEffect/Layout.jsx';
+import { Memorize } from './06-memos/Memorize.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout/>
+    <Memorize/>
   </React.StrictMode>,
 )
