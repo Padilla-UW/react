21fd60bb8c13428efd8b37f361a8aaef007e3238
Padilla <hector.padilla@uwipes.com>
2022-12-28 16:08:06 -0600

    tarea mem

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CallbackHook } from './06-memos/CallbackHook.jsx';
+//import { CallbackHook } from './06-memos/CallbackHook.jsx';
 //import { MemoHook } from './06-memos/MemoHook.jsx';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/counterWithCustomHook';
@@ -11,11 +11,12 @@ import { CallbackHook } from './06-memos/CallbackHook.jsx';
 //import { FocusScreen } from './04-useRef/FocusScreen.jsx';
 //import { Layout } from './05-useLayoutEffect/Layout.jsx';
 //import { Memorize } from './06-memos/Memorize.jsx';
+import { Padre } from './07-tarea-memo/Padre.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
   </React.StrictMode>,
 )
