0d1f7200b1f441e225f05fe43f50fdcce7f0dab7
Padilla <hector.padilla@uwipes.com>
2023-01-12 17:48:55 -0600

    instalacion react router

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 //import { CallbackHook } from './06-memos/CallbackHook.jsx';
 //import { MemoHook } from './06-memos/MemoHook.jsx';
 //import { CounterApp } from './01-useState/CounterApp';
@@ -13,11 +18,16 @@ import ReactDOM from 'react-dom/client';
 //import { Memorize } from './06-memos/Memorize.jsx';
 //import { Padre } from './07-tarea-memo/Padre.jsx';
 //import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp.jsx';
+//import { TodoApp } from './08-useReducer/TodoApp.jsx';
+import { MainApp } from './09-useContext/MainApp.jsx';
 import './index.css';
 
+MainApp
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp/>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <MainApp/>
+        </BrowserRouter>
+    </React.StrictMode>
+
 )
